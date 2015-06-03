8d61f746480f3e7c7029250c31a2ac4b8d6c6ec9
Brock Neilson <brockneilson@Brocks-MacBook-Pro-2.local>
2015-06-03 10:07:51 -0600

    Project Update

---
+++ js/controllers/mainCtrl.js
@@ -0,0 +1,40 @@
+var app = angular.module('quoteBook');
+
+app.controller('mainCtrl', function($scope, mainService){
+
+	$scope.showAdds = false;
+	$scope.showRemoves = false;
+	$scope.showFilters = false;
+
+	$scope.showAdd = function(){
+		$scope.showAdds = !$scope.showAdds;
+		$scope.showRemoves = false;
+		$scope.showFilters = false;
+	}
+
+	$scope.showRemove = function(){
+		$scope.showRemoves = !$scope.showRemoves;
+		$scope.showAdds = false;
+		$scope.showFilters = false;
+	}
+
+	$scope.showFilter = function(){
+		$scope.showFilters = !$scope.showFilters;
+		$scope.showAdds = false;
+		$scope.showRemoves = false;
+	}
+
+	$scope.getData = function(){
+		$scope.quotes = mainService.getData();
+	};
+	$scope.getData();
+
+	$scope.addData = function(){
+		mainService.addData($scope.quoteAdd, $scope.authorAdd);
+	};
+
+	$scope.removeData = function(){
+		mainService.removeData($scope.quoteRemove);
+	};
+
+});
